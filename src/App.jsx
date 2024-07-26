134d1956744f67418bb503c4f00869dcd7d7b403
Juan Ledesma <jueleto@gmail.com>
2024-07-26 01:09:33 +0000

    se agregar uso de axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+
+
+  const [valorHola, setValorHola] = useState("Hola");
+  const [valorCantidad, setValorCantidad] = useState(0);
+
+
+  const [arrayPaises, setArrayPaises] = useState([]);
+
+  //https://restcountries.com/
+  const requestPaises = () => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        // handle success
+        //console.log(response);
+        
+        //console.log("data", );
+
+        setArrayPaises(response.data);
+
+        for(let i=0; i< response.data.length; i++){
+            const dataActual=response.data[i];
+            console.log(i+" ", dataActual);
+            console.log("name ", dataActual.name.common);
+            console.log("flags ", dataActual.flags.png);
+        }
+
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+
+  }
 
 
 
@@ -13,7 +51,30 @@ function App() {
       </div>
       <h1>Mi primera app react</h1>
       <h2>Mi primer cambio</h2>
-     
+      <h3>{"valorHola: " + valorHola + " "+valorCantidad}</h3>
+      <button onClick={requestPaises}> Obtener todos los paises</button>
+      <button
+        onClick={() => {
+          setValorHola("Hola kp");
+          setValorCantidad(valorCantidad+1);
+        }
+        }
+      > Setear Valor Hola</button>
+
+      {/* <p>{JSON.stringify(arrayPaises)}</p> */}
+
+      {arrayPaises.map((item, index)=>{
+
+        return(
+          <div key={"k-"+index}>
+          <h6>{item.name.common}</h6>
+          <img src={item.flags.png}/>
+          </div>
+        )
+      })
+
+      }
+
     </>
   )
 }
